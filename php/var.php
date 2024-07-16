19f043c703d170ca14bfdc58ae5f208b4a3182fb
oktayudha05 <oktayudha05@gmail.com>
2024-07-16 13:45:36 +0700

    latihan php

---
+++ php/var.php
@@ -0,0 +1,10 @@
+  <?php
+  $tahun_lahir = 2005;
+  $tahun_sekarang = 2024;
+  $nama = "Budi";
+  $status = "Pelajar";
+  $umur = $tahun_sekarang - $tahun_lahir;
+
+  echo $nama;
+  echo "<br>";
+  echo $umur;
